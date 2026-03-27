3a61cab475b4977bb728480175e2e52f622739d6
adm-amarsetti <andrea.marsetti@zech-hochbau.de>
2026-03-27 13:06:13 +0100

    Added language support (only resx files)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 using BoardGames;
 using BoardGames.Components;
 using LumexUI.Extensions;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +11,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+var supportedCultures = new[] { "en", "it" };
+
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture("it")
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+localizationOptions.RequestCultureProviders.Insert(0,
+    new CookieRequestCultureProvider());
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=app.db"));
 
 builder.Services.AddLumexServices();
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("it");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("it");
 
 var app = builder.Build();
 
@@ -23,6 +40,8 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseAntiforgery();
+
+app.UseRequestLocalization(localizationOptions);
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
