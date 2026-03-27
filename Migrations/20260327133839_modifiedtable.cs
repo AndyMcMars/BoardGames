ad2626220352debfc1662ad55585f81b6478e4be
adm-amarsetti <andrea.marsetti@zech-hochbau.de>
2026-03-27 14:39:47 +0100

    added migrations and migrations apply on startup

---
+++ Migrations/20260327133839_modifiedtable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoardGames.Migrations
+{
+    /// <inheritdoc />
+    public partial class modifiedtable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Locations_LocationId",
+                table: "Matches");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "Matches",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Locations_LocationId",
+                table: "Matches",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Locations_LocationId",
+                table: "Matches");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "Matches",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Locations_LocationId",
+                table: "Matches",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id");
+        }
+    }
+}
