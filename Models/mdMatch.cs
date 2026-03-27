25e932f9d3849267182c3d417b21d4f1c51d57b0
adm-amarsetti <andrea.marsetti@zech-hochbau.de>
2026-03-27 12:45:17 +0100

    added icons, main game match page and match page

--- Models/mdMatch.cs
+++ Models/mdMatch.cs
@@ -7,7 +7,7 @@ public class mdMatch
     public int? TournamentId { get; set; }
     public mdTournament? Tournament { get; set; }
 
-    public int? LocationId { get; set; }
+    public int LocationId { get; set; }
     public mdLocation? Location { get; set; }
 
     public int GameId { get; set; }
@@ -18,4 +18,12 @@ public class mdMatch
 
     public List<mdResult> Results { get; set; } = new();
     public List<mdReview> Reviews { get; set; } = new();
+
+    public mdResult? Winner =>
+        Results.OrderByDescending(r => r.Points).FirstOrDefault();
+    public double? Stars =>
+        Reviews.Any()
+            ? Math.Round(Reviews.Average(r => r.Stars), 1)
+            : 0;
+
 }
